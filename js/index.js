1b6407514628089b78a70cf0a3c20667a0ed4eda
skyetian <ziqint@uw.edu>
2020-11-14 01:22:19 +0800

    project1

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 function search() {
 	let searched = document.getElementById("search").value.trim();
 	if(searched !== "") {
-  	    let text = document.getElementById("text").innerHTML;
-  		let re = new RegExp(searched,"g"); // search for all instances
+		let text = document.getElementById("text").innerHTML;
+		let re =  new RegExp(search, "g"); // search for all instances
 		let newText = text.replace(re, `<mark>${searched}</mark>`);
 		document.getElementById("text").innerHTML = newText;
     }
