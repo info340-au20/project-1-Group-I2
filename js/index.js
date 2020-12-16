edb6051714d2b872f6849ff79b380c67c0c48d57
SinanZhao <cielzhao1013@gmail.com>
2020-12-15 19:53:24 -0800

    fix bugs

--- js/index.js
+++ js/index.js
@@ -1,50 +1,18 @@
 'use strict';
+/* global d3*/
 
 function search() {
 	let searched = document.getElementById("search").value.trim();
 	if(searched !== "") {
-  	    let text = document.getElementById("text").innerHTML;
-  		let re = new RegExp(searched,"g"); // search for all instances
+		let text = document.getElementById("text").innerHTML;
+		let re = new RegExp(searched,"g"); // search for all instances
 		let newText = text.replace(re, `<mark>${searched}</mark>`);
 		document.getElementById("text").innerHTML = newText;
-    }
+	}
 }
 document.querySelector('#highlight').onclick = function() {
 	search();
 }
-/*
-let demo = [
-	{"race":"white", "new_cancer_rate":438.8, "death_rate":153.6},
-	{"race":"black", "new_cancer_rate":429.1, "death_rate":173.7},
-	{"race":"Asian/Pacific Islander", "new_cancer_rate":284.2, "death_rate":95.6},
-	{"race":"Amrican Indian/Alaska Native", "new_cancer_rate":269, "death_rate":100.1},
-	{"race":"Hispanic", "new_cancer_rate":333, "death_rate":109.5}
-];
-let alcohol = [
-	{"cancer_type":"all", "alcohol-associated":128.9},
-	{"cancer_type":"Female Breast", "alcohol-associated":125.1},
-	{"cancer_type":"Colon and Rectum", "alcohol-associated":36.8},
-	{"cancer_type":"Lip, Oral Cavity and Pharynx", "alcohol-associated":11.7},
-	{"cancer_type":"Liver", "alcohol-associated":6.8},
-	{"cancer_type":"Esophagus", "alcohol-associated":4.4},
-	{"cancer_type":"Larynx", "alcohol-associated":3}
-];
-let tobacco = [
-	{"cancer_type":"all", "":181.1},
-	{"cancer_type":"Trachea, Lung and Bronchus", "tobacco-associated":55.3},
-	{"cancer_type":"Colon and Rectum", "tobacco-associated":36.8},
-	{"cancer_type":"Urinary Bladder", "tobacco-associated":19.1},
-	{"cancer_type":"Kidney and Renal Pelvis", "tobacco-associated":16.9},
-	{"cancer_type":"Pancreas", "tobacco-associated":12.9},
-	{"cancer_type":"Lip, Oral Cavity and pharynx", "tobacco-associated":11.7},
-	{"cancer_type":"Cervix", "tobacco-associated":7.5},
-	{"cancer_type":"Liver", "tobacco-associated":6.8},
-	{"cancer_type":"Stomach", "tobacco-associated":6.3},
-	{"cancer_type":"Esophagus", "tobacco-associated":4.4},
-	{"cancer_type":"Acute Myeloid Leukemia", "tobacco-associated":4.1},
-	{"cancer_type":"Larynx", "tobacco-associated":3.0}
-];
-*/
 
 // when clicking the button of know more about demographic, show some statistics
 let buttonDemo = document.querySelector('#demobutton');
@@ -130,9 +98,9 @@ function clickRisk(){
 	}
 
 	if (type.value == "1"){
-		document.getElementById("here1").innerHTML='<img src="img/risk_alchohol.jpg" width = 100% height = 100%>';
+		document.getElementById("here1").innerHTML='<img src="img/risk_alchohol.jpg">';
 	} else {
-		document.getElementById("here1").innerHTML='<img src="img/risk_tobacco.jpg" width = 100% height = 100%>';
+		document.getElementById("here1").innerHTML='<img src="img/risk_tobacco.jpg">';
 	}
 
 }
@@ -147,8 +115,8 @@ function clickDemo(){
 	}
 
 	if (type.value == "1"){
-		document.getElementById("here2").innerHTML='<img src="img/rateofnewdeath.jpg" width = 100% height = 100%>';
+		document.getElementById("here2").innerHTML='<img src="img/rateofnewdeath.jpg">';
 	} else  {
-		document.getElementById("here2").innerHTML='<img src="img/rate_cancer.jpg" width = 100% height = 100%>';
+		document.getElementById("here2").innerHTML='<img src="img/rate_cancer.jpg">';
 	}
 }
